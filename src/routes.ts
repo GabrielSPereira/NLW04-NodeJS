d36b7a593adb6134f79848de2124a7fadb1d7f67
Gabriel Souza Pereira <gabrielspereira2604@gmail.com>
2021-02-28 17:42:32 -0300

    Add Nps

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswersController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveyController();
 const sendMailController = new SendMailController();
+const answersController = new AnswersController();
+const npsController = new NpsController();
 
 router.post('/users', userController.create);
 
@@ -16,5 +20,9 @@ router.post('/surveys', surveysController.create);
 router.get('/surveys', surveysController.show);
 
 router.post('/sendMail', sendMailController.execute);
+
+router.get('/answers/:value', answersController.execute);
+
+router.get('/nps/:survey_id', npsController.execute);
 
 export { router };
